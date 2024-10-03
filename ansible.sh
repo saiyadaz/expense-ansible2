051a5ab34daf6ddfcd74b9b44a144344242ed5c9
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-03 11:05:07 -0400

    edit pipelines

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
-component= $1
+component=$1
 ansible-playbook -i  $component-{{ env }}.devopssz14.online -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
