999d54ba6f35b79dba8741399c4f9e24c844197b
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-16 19:29:51 -0400

    edit ansible.sh for secrets

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,3 @@
-component=$1
-
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
-ansible-playbook -i $component-$env.devopssz14.online, -e env=$env -e role_name=$component expense.yml -e '@secrets.json' -e '@app.json'
+ansible-playbook -i $component-$env.devopssz14.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
 #ansible-playbook -i  $component-$env.devopssz14.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
