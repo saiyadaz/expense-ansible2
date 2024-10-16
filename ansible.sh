560fee8586dccea0b8fcc85aa723e8f80d83e3d1
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-16 18:43:54 -0400

    add get-secrets.yml for vault token and yaml file

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,4 @@
 component=$1
-ansible-playbook -i  $component-$env.devopssz14.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
+
+ansible-playbook get-secrets.yml -e vault_token=$vault_token
+#ansible-playbook -i  $component-$env.devopssz14.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
