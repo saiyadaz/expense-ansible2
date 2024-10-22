3f60d47cfb5aa79ad6d7885a03464376e964b1fc
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-22 13:38:16 -0400

    add to ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,10 @@
 component=$1
-
+rm -f ~/*.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
-ansible-playbook -i $component-$env.devopssz14.online, -e env=$env -e role_name=$component expense.yml -e '@secrets.json' -e '@app.json'
+#ansible-playbook -i $component-$env.devopssz14.online, -e env=$env -e role_name=$component expense.yml -e '@secrets.json' -e '@app.json'
 
+
+aws ec2 describe-instances --filters Name=tag:Name,Values=$component-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
+ansible-playbook -i inv -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
+
+rm -f ~/*.json
